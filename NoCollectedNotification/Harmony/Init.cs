e35b31987957261f34b67cb26be491f6bcf985ad
Rizart Dokollari <r.dokollari@gmail.com>
2024-08-15 23:13:26 +0100

    chore: Initialise project.

---
+++ NoCollectedNotification/Harmony/Init.cs
@@ -0,0 +1,13 @@
+﻿namespace NoCollectedNotification.Harmony
+{
+    public class Init : IModApi
+    {
+        private static readonly ILogger Logger = new Logger();
+
+        public void InitMod(Mod modInstance)
+        {
+            var harmony = new HarmonyLib.Harmony("uk.co.rdok.7daystodie.mod.no_collected_notification");
+            harmony.PatchAll();
+        }
+    }
+}
